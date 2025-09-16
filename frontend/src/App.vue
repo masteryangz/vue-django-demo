3b232496efa091f4cc4cc9bd273e87d7a9fcbb0c
Masteryangz <yangzuo2001@gmail.com>
2025-09-16 13:55:11 +0000

    simple done

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,23 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+import { ref, onMounted } from "vue";
+import axios from "axios";
+
+const message = ref("Loading...");
+
+onMounted(async () => {
+  try {
+    const res = await axios.get("http://127.0.0.1:8000/api/hello/");
+    message.value = res.data.message;
+  } catch (err) {
+    message.value = "Error: " + err.message;
+  }
+});
 </script>
 
 <template>
+  <h1>{{ message }}</h1>
   <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
